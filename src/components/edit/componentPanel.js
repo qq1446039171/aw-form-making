9688358cc3d03412885993adf20838d130656135
黄博文 <1446039171@qq.com>
2022-12-02 15:37:38 +0800

    正则匹配

--- src/components/edit/componentPanel.js
+++ src/components/edit/componentPanel.js
@@ -1,3 +1,8 @@
-const editComponentPanelMap = new Map([['input', () => import('@/components/edit/input.vue')]])
+const components = require.context('./', false, /\.vue$/)
+let editComponent = components.keys().map((i) => {
+  let key = i.match(/\/(\S*)\./)[1]
+  return [key, () => import(`@/components/edit/${key}.vue`)]
+})
+const editComponentPanelMap = new Map(editComponent)
 
 export default editComponentPanelMap
